9e1093a25e20916b396551708b29a16aa39dd48c
Nathan <nathan@hurel.me>
2016-08-12 16:56:28 +0200

    Added small hack to generation completion script from command line

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
-/*import (
-	"github.com/nhurel/dim/lib"
-	"flag"
-	"github.com/Sirupsen/logrus"
-)*/
 import (
 	"fmt"
 	"github.com/nhurel/dim/cmd"
 	"os"
 )
 
-func main() {
+var GenerateCompletion string
 
+func main() {
+	if GenerateCompletion == "true" {
+		complete()
+		return
+	}
 	if err := cmd.RootCommand.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	os.Exit(0)
 
+}
+
+func complete() {
+	cmd.RootCommand.GenBashCompletionFile("dim_compl")
 }
